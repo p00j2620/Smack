385d7b560a145f087d14a907c5a70245677fbafc
Art Pujols <p00j2620@gmail.com>
2017-08-06 16:55:46 -0400

    Setting up auth service and global constants. Completed registration function.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+// URL Constants
+let BASE_URL = "https://chatapideploy.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLoginVCSegue"
 let TO_ACCOUNT = "toAccountVCSegue"
 let UNWIND_TO_CHANNELVC = "unwindToChannelVC"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
