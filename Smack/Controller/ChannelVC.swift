a0fd365bcf4c761e47f575b714a23c5fc0af9d9a
Art Pujols <p00j2620@gmail.com>
2017-08-18 01:03:47 -0400

    Hooked up table view in Channel VC. Displaying properly

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 // This VC controls the front view in SWReveal
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	//Outlets
 	@IBOutlet weak var loginBtn: UIButton!
 	@IBOutlet weak var avatarImage: CircleUIImage!
+	@IBOutlet weak var tableView: UITableView!
 	
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		tableView.delegate = self
+		tableView.dataSource = self
 		self.dismissKeyboardOnTap()
 		NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
 		// Setting the width of revealed window - ChatVC
@@ -57,4 +60,24 @@ class ChannelVC: UIViewController {
 			avatarImage.backgroundColor = UIColor.clear
 		}
 	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+			let channel = MessageService.instance.channels[indexPath.row]
+			cell.configureCell(channel: channel)
+			return cell
+		} else {
+			return UITableViewCell()
+		}
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return MessageService.instance.channels.count
+	}
+	
+	
+	
 }
